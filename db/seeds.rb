f48728d0bdce00b7f12678993681cf0de4b1e340
Malo Bobrowska <malo.bobrow@gmail.com>
2023-02-24 12:50:20 +0100

    fix deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Movie.destroy_all
+
+puts 'Creating 5 fake movies...'
+5.times do
+  movie = Movie.create!(
+    title: Faker::Movie.title,
+    overview: Faker::Quote.yoda,
+    poster_url: Faker::Internet.url,
+    rating: rand(0..10)
+  )
+  # puts "Created:#{movie.title}, #{movie.overview}, #{movie.poster_url}, #{movie.rating}"
+end
